85faa24f0fb15c2122daa82b8a9f84c6acf89167
DesertDroid <nikolaynikolovx@gmail.com>
2014-09-23 11:17:21 +0300

    Changing local to en_US

--- plot3.R
+++ plot3.R
@@ -1,3 +1,6 @@
+## Ensure the locale is en_US
+Sys.setlocale("LC_ALL", "en_US.UTF-8")
+
 ## We check whether it has the file in the current dir.
 if (!"load_data.R" %in% list.files()) {
         setwd("~/repos/Coursera/dataScience/ExploratoryDataAnalysis/ExData_Plotting1")
